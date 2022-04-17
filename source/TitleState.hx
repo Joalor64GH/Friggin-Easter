bf0bd69b9e7fba660133764249c5a6262cd75329
Joalor64 <rhgarcon@gmail.com>
2022-04-17 12:45:24 -0400

    why lol

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -575,16 +575,16 @@ class TitleState extends MusicBeatState
 			{
 				case 1:
 					#if PSYCH_WATERMARKS
-					createCoolText(['Psych Engine by'], 15);
+					createCoolText(['Made by'], 15);
 					#else
 					createCoolText(['ninjamuffin99', 'phantomArcade', 'kawaisprite', 'evilsk8er']);
 					#end
 				// credTextShit.visible = true;
 				case 3:
 					#if PSYCH_WATERMARKS
-					addMoreText('Shadow Mario', 15);
-					addMoreText('RiverOaken', 15);
-					addMoreText('shubs', 15);
+					addMoreText('Joalor64', 15);
+					addMoreText('Nobody Else', 15);
+					addMoreText('LOL', 15);
 					#else
 					addMoreText('present');
 					#end
@@ -597,7 +597,7 @@ class TitleState extends MusicBeatState
 				// credTextShit.screenCenter();
 				case 5:
 					#if PSYCH_WATERMARKS
-					createCoolText(['Not associated', 'with'], -40);
+					createCoolText(['I am in love', 'with'], -40);
 					#else
 					createCoolText(['In association', 'with'], -40);
 					#end
@@ -624,13 +624,13 @@ class TitleState extends MusicBeatState
 				// credTextShit.text = "Friday";
 				// credTextShit.screenCenter();
 				case 13:
-					addMoreText('Friday');
+					addMoreText('Dumb');
 				// credTextShit.visible = true;
 				case 14:
-					addMoreText('Night');
+					addMoreText('Easter');
 				// credTextShit.text += '\nNight';
 				case 15:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('Special'); // credTextShit.text += '\nFunkin';
 
 				case 16:
 					skipIntro();
